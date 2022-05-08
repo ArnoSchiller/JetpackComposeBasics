5d8442b6d1957cb5d077d48c34abeef01c70295a
ArnoSchiller <arno.schiller@student.jade-hs.de>
2022-05-08 16:35:54 +0200

    Initial navigation setup

--- AndroidComposeBasics/app/src/main/java/de/byschiller/androidcomposebasics/MainActivity.kt
+++ AndroidComposeBasics/app/src/main/java/de/byschiller/androidcomposebasics/MainActivity.kt
@@ -3,14 +3,30 @@ package de.byschiller.androidcomposebasics
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.Menu
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import de.byschiller.androidcomposebasics.ui.theme.AndroidComposeBasicsTheme
+import kotlinx.coroutines.launch
+
+/**
+ * Initial navigation setup source: https://proandroiddev.com/navigation-drawer-using-jetpack-compose-27ea7db74903
+ */
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,22 +38,143 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    AppMainScreen()
                 }
             }
         }
     }
 }
 
+
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun AppMainScreen() {
+    val navController = rememberNavController()
+    Surface(color = MaterialTheme.colors.background) {
+        val drawerState = rememberDrawerState(DrawerValue.Closed)
+        val scope = rememberCoroutineScope()
+        val openDrawer = {
+            scope.launch {
+                drawerState.open()
+            }
+        }
+        ModalDrawer(
+            drawerState = drawerState,
+            gesturesEnabled = drawerState.isOpen,
+            drawerContent = {
+                Drawer(
+                    onDestinationClicked = { route ->
+                        scope.launch {
+                            drawerState.close()
+                        }
+                        navController.navigate(route) {
+                            popUpTo = navController.graph.startDestinationId
+                            launchSingleTop = true
+                        }
+                    }
+                )
+            }
+        ) {
+            NavHost(
+                navController = navController,
+                startDestination = DrawerScreens.Home.route
+            ) {
+                composable(DrawerScreens.Home.route) {
+                    Home(
+                        openDrawer = {
+                            openDrawer()
+                        }
+                    )
+                }
+                composable(DrawerScreens.Account.route) {
+                    Account(
+                        openDrawer = {
+                            openDrawer()
+                        }
+                    )
+                }
+                composable(DrawerScreens.Help.route) {
+                    Help(
+                        navController = navController
+                    )
+                }
+            }
+        }
+    }
+}
+
+
+@Composable
+fun TopBar(title: String = "", buttonIcon: ImageVector, onButtonClicked: () -> Unit) {
+    TopAppBar(
+        title = {
+            Text(
+                text = title
+            )
+        },
+        navigationIcon = {
+            IconButton(onClick = { onButtonClicked() } ) {
+                Icon(buttonIcon, contentDescription = "")
+            }
+        },
+        backgroundColor = MaterialTheme.colors.primaryVariant
+    )
+}
+
+@Composable
+fun Home(openDrawer: () -> Unit) {
+    Column(modifier = Modifier.fillMaxSize()) {
+        TopBar(
+            title = "Home",
+            buttonIcon = Icons.Filled.Menu,
+            onButtonClicked = { openDrawer() }
+        )
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally) {
+            Text(text = "Home Page content here.")
+        }
+    }
+}
+
+@Composable
+fun Account(openDrawer: () -> Unit) {
+    Column(modifier = Modifier.fillMaxSize()) {
+        TopBar(
+            title = "Account",
+            buttonIcon = Icons.Filled.Menu,
+            onButtonClicked = { openDrawer() }
+        )
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally) {
+            Text(text = "Account.", style = MaterialTheme.typography.h4)
+        }
+    }
+}
+
+@Composable
+fun Help(navController: NavController) {
+    Column(modifier = Modifier.fillMaxSize()) {
+        TopBar(
+            title = "Help",
+            buttonIcon = Icons.Filled.ArrowBack,
+            onButtonClicked = { navController.popBackStack() }
+        )
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally) {
+            Text(text = "Help.", style = MaterialTheme.typography.h4)
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     AndroidComposeBasicsTheme {
-        Greeting("Android")
+        AppMainScreen()
     }
 }
